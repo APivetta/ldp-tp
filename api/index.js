4b4bbb50708d02ad305781b63eb3fa0611f13b9a
Agustin Pivetta <aguspivetta@outlook.com>
2016-12-16 14:29:28 -0300

    cors enables

--- api/index.js
+++ api/index.js
@@ -24,6 +24,12 @@ router.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /*
 Ruta						Verbo HTTP	Descripcion
 /api/authors				GET			Lista todos los autores
